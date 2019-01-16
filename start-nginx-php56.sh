c1d12bfec39fdff10f12f56815c3d9d474407891
edwardsbean <edwardsbean@gmail.com>
2019-01-16 17:27:33 +0800

    add php fpm status

--- start-nginx-php56.sh
+++ start-nginx-php56.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # 修改.env文件，来设置 
+docker network create my-bridge
 docker-compose -f nginx-php56.yml up
--- start-nginx-php72.sh
+++ start-nginx-php72.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # 修改.env文件，来设置 
+docker network create my-bridge
 docker-compose -f nginx-php72.yml up
