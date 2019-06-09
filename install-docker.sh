6c69bb37e4af673d9d9a9885cf76e6c3893cab34
edwardsbean <edwardsbean@gmail.com>
2019-06-09 17:09:44 +0800

    fix

--- install-docker.sh
+++ install-docker.sh
@@ -15,6 +15,10 @@ sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install -y docker-ce docker-ce-cli containerd.io    
+sudo yum install -y docker-ce docker-ce-cli containerd.io 
 
 sudo systemctl start docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
--- start-nginx-php-mysql.sh
+++ start-nginx-php-mysql.sh
@@ -1,2 +1,3 @@
 docker network create my-bridge
 # .env文件中修改mysql等参数
+docker-compose -f npm.yml up
